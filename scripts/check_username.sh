bfce782e90f8a812f076ece6785002a70ae3c727
Abhay Riyal <107146896+Abhay-Riyal@users.noreply.github.com>
2024-01-30 20:51:04 +0530

    Update check_username.sh
    changed the option from w to x as it matches the whole line

--- scripts/check_username.sh
+++ scripts/check_username.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cat ./records/account.txt | awk '{print $3}' > ./records/temp.txt 
-grep -w -c "\b$1\b" ./records/temp.txt > ./records/exist.txt
+grep -x -c "\b$1\b" ./records/temp.txt > ./records/exist.txt
