2165da5e0aa257ed9bdac930672bf9c6d4314461
Abhay Riyal <107146896+Abhay-Riyal@users.noreply.github.com>
2024-04-25 00:07:44 +0530

    Update check_username.sh

--- scripts/check_username.sh
+++ scripts/check_username.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-cat ./records/account.txt | awk '{print $3}' > ./records/temp.txt 
-grep -x -c $1 ./records/temp.txt > ./records/exist.txt
+cat ./records/account.txt | awk '{print $3}' | grep -xc > ./records/exist.txt
